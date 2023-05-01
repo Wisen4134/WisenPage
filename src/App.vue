ae6ad9fd1a1b563b5317bbc36daacfbd493abf1f
Wisen <break4134@gmail.com>
2023-05-02 00:07:08 +0800

    *Version 0.0.2     *配置完test.vue的nav部分     *TODO         *將切入點網頁更改         *router需再設置     *issue         *avarator的圖片顯示不出來

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <!--
+    <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,6 +18,18 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
+  -->
+  <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/test">Test</RouterLink>
+      </nav>
+    </div>
 
   <RouterView />
 </template>
