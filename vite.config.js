181a8258ed3fd843a208d07650cc255f383bfbe4
wisen <wisen.huang@acculite.com.tw>
2023-06-19 17:13:04 +0800

    vite.config.js修改

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //base => 對應gitpage上的專案名稱,打包後所產生之dist內容路徑需要以此名稱開頭
   base:'/WisenPage/',
   resolve: {
     alias: {
